1f6bf62d405b4231f431b15ba411f09bdd50f100
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-12 19:57:14 +0200

    first part milestone-2

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -3,16 +3,43 @@
         <div class="card d-flex flex-column align-items-center h-100 p-3">
             <div>{{ movies.title }}</div>
             <div>{{ movies.title }}</div>
-            <div>{{ movies.original_language }}</div>
+            <!-- <div>{{ movies.original_language }}</div> -->
+
+            <!-- Milestone 2:
+            Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente,gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API. -->
+            <lang-flag :iso="movies.original_language" v-if="setFlag" />
+            <div class="text-center" v-else>
+                <p>no language available
+                    <!--{{movies.original_language}}-->
+                </p>
+            </div>
             <div>{{ movies.vote_average }}</div>
         </div>
     </div>
 </template>
 <script>
-import MovieCard from './MovieCard.vue';
+
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
-    name: MovieCard,
-    props: ['movies']
+    name: 'MovieCard',
+    components: {
+        LangFlag,
+    },
+    props: ['movies'],
+    data() {
+        return {
+            original_language: '',
+        }
+    },
+    computed: {
+        setFlag() {
+            if (this.movies.original_language === 'sv' || this.movies.original_language === 'no') {
+                return false;
+            }
+            return true;
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
